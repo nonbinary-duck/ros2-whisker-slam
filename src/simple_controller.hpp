e656eac49e8536b92f602665c65b8b4369517851
nonbinary-duck <46252182+nonbinary-duck@users.noreply.github.com>
2024-01-30 21:29:26 +0000

    Removed commented out copy-paste code from the docs

--- src/simple_controller.hpp
+++ src/simple_controller.hpp
@@ -123,30 +123,6 @@ namespace simple_ros2_test
     };
 
 
-//     class MinimalPublisher : public rclcpp::Node
-// {
-// public:
-//   MinimalPublisher()
-//   : Node("minimal_publisher"), count_(0)
-//   {
-//     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-//     auto timer_callback =
-//       [this]() -> void {
-//         auto message = std_msgs::msg::String();
-//         message.data = "Hello, world! " + std::to_string(this->count_++);
-//         RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-//         this->publisher_->publish(message);
-//       };
-//     timer_ = this->create_wall_timer(500ms, timer_callback);
-//   }
-
-// private:
-//   rclcpp::TimerBase::SharedPtr timer_;
-//   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-//   size_t count_;
-// };
-
-
 } // End namespace simple-ros2-test
 
 #endif // H_599955_SRC_SIMPLE_CONTROLLER
